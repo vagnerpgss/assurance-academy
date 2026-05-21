c1392e4ebde15a13e3745f689f26f95be4f919e7
Roberto Vilas Boas <robertovilasboas@Robertos-MacBook-Air.local>
2026-05-21 15:58:48 -0300

    many integrations development

---
+++ src/main/java/com/aws/class3/dynamo/controller/DynamoController.java
@@ -0,0 +1,39 @@
+package com.aws.class3.dynamodb.controller;
+
+import com.aws.class3.dynamodb.service.DynamoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/dynamodb")
+public class DynamoController {
+
+    private final DynamoService service;
+
+    public DynamoController(DynamoService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> save(
+            @RequestParam String id,
+            @RequestParam String nome
+    ) {
+        return ResponseEntity.ok(service.save(id, nome));
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Map<String, String>> find(@PathVariable String id) {
+        return ResponseEntity.ok(service.find(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(
+            @PathVariable String id
+    ) {
+        return ResponseEntity.ok(service.delete(id));
+    }
+}
---
+++ src/main/java/com/aws/class3/sqs/config/SqsConfig.java
@@ -0,0 +1,19 @@
+package com.aws.class3.sqs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Configuration
+public class SqsConfig {
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.SA_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}

27bd27d1bce5200514c45cb52a58a82976cd4de0
Vagner Santos <vgoncalvesda@contractor.indeed.com>
2026-06-25 22:27:21 -0300

    dynamodb

--- src/main/java/com/aws/class3/sql/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aws.class3.sql.repository;
-
-import com.aws.class3.sql.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
